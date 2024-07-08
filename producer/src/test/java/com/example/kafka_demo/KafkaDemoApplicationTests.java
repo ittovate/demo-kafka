d6d275f319e593e0475ef124d03d3f9b3f54ea2e
Mohanad <mohanadkhaled87@gmail.com>
2024-07-08 18:42:06 +0300

    added initial producer and consumer projects

---
+++ producer/src/test/java/com/example/kafka_demo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
