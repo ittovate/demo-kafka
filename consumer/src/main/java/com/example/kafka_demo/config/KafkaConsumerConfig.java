7cc24aca3f0d2b24da363423cfc10312a204cc6e
Mostafa Hussien <mostafahass314@gmail.com>
2024-09-13 16:16:09 +0300

    implement : schema registerer to Consumer

--- consumer/src/main/java/com/example/kafka_demo/config/KafkaConsumerConfig.java
+++ consumer/src/main/java/com/example/kafka_demo/config/KafkaConsumerConfig.java
@@ -2,11 +2,10 @@ package com.example.kafka_demo.config;
 
 
 import com.example.kafka_demo.model.Person;
+import com.example.kafka_demo.model.generated.Order;
+import com.example.kafka_demo.model.generated.PersonAvro;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +13,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,17 +24,72 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${spring.kafka.integer-deserializer}")
-    private String integerDeserializer;
-
+   @Value("${spring.kafka.integer-deserializer}")
+   private String integerDeserializer;
     @Value("${spring.kafka.string-deserializer}")
     private String stringDeserializer;
 
-    @Value("${spring.kafka.json-deserializer}")
-    private String jsonDeserializer;
+   @Value("${spring.kafka.json-deserializer}")
+   private String jsonDeserializer;
 
     @Value("${spring.kafka.consumer-group-id}")
     private String consumerGroupId;
+
+    @Value("${spring.kafka.specific-avro-reader}")
+    private String specificAvroReader;
+
+    @Value("${spring.kafka.schema-registry-url}")
+    private String schemaRegistryUrl;
+
+    @Value("${spring.kafka.autoOffsetReset}")
+    private String autoOffsetReset;
+
+    @Value("${spring.kafka.Value-Avro-Deserializer}")
+    private String ValueAvroDeserializer;
+
+    @Bean
+    public ConsumerFactory<Integer,String> stringConsumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(stringConsumerProperties());
+    }
+    @Bean
+    public ConsumerFactory<Integer,Person> jsonConsumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(jsonConsumerProperties());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Order> orderAvroKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Order> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(avroConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PersonAvro> personAvroKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PersonAvro> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(avroConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public <T> ConsumerFactory<String, Object> avroConsumerFactory() {
+        Map<String, Object> consumerProperties = new HashMap<>();
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
+
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, stringDeserializer );
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ValueAvroDeserializer );
+
+        consumerProperties.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl );
+
+        consumerProperties.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, specificAvroReader );
+
+        consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset );
+
+        return new DefaultKafkaConsumerFactory<>(consumerProperties);
+    }
+
+
+
+
 
     Map<String,Object> stringConsumerProperties(){
         Map<String,Object> consumerProperties = new HashMap<>();
@@ -54,18 +106,11 @@ public class KafkaConsumerConfig {
         consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, integerDeserializer);
         consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, jsonDeserializer);
         consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG,consumerGroupId);
-        consumerProperties.put(JsonDeserializer.TRUSTED_PACKAGES,"com.example.kafka_demo.model");
         return consumerProperties;
     }
 
-    @Bean
-    public ConsumerFactory<Integer,String> stringConsumerFactory(){
-        return new DefaultKafkaConsumerFactory<>(stringConsumerProperties());
-    }
-    @Bean
-    public ConsumerFactory<Integer,Person> jsonConsumerFactory(){
-        return new DefaultKafkaConsumerFactory<>(jsonConsumerProperties());
-    }
+
+
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>> stringKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<Integer, String> factory
@@ -73,6 +118,8 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(stringConsumerFactory());
         return factory;
     }
+
+
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, Person>> jsonKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<Integer, Person> factory
@@ -80,5 +127,6 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(jsonConsumerFactory());
         return factory;
     }
+
 
 }
