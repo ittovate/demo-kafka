fd766b32a8bbe3b94d2001244d869c2423aa7e55
Mostafa Hussien <mostafahass314@gmail.com>
2024-09-13 16:20:12 +0300

    handle : topic names for use in listening

--- consumer/src/main/java/com/example/kafka_demo/util/KafkaUtils.java
+++ consumer/src/main/java/com/example/kafka_demo/util/KafkaUtils.java
@@ -1,11 +1,29 @@
 package com.example.kafka_demo.util;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KafkaUtils {
-    @Value("${spring.kafka.default-topic}")
-    public static String demoTopic;
+   @Value("${spring.kafka.topic.default-topic}")
+   private  String demoTopic;
 
     @Value("${spring.kafka.consumer-group-id}")
-    public static String groupId;
+    private  String groupId;
+
+    @Value("${spring.kafka.topic.person-topic}")
+    private   String  personTopic ;
+
+    public  String getDemoTopic() {
+        return demoTopic;
+    }
+
+    public  String getGroupId() {
+        return groupId;
+    }
+
+    public  String getPersonTopic() {
+        return personTopic;
+    }
 }
